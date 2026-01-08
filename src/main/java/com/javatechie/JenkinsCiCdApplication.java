a72152287762bd23a136b7d7379746ce90ef9505
thesn <thesnehasismondal@gmail.com>
2026-01-08 21:18:27 +0100

    test

---
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -0,0 +1,26 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsCiCdApplication {
+
+
+    @GetMapping("/greetings/{name}")
+    public String greetings(@PathVariable String name) {
+        //Basant kumar hota
+        //sarvindra kumar
+       name= name.split(" ")[0];
+        return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCdApplication.class, args);
+    }
+
+}
